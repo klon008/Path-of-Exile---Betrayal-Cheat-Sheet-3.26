718c469a9ac3f1ce6dea881526d582c0f07936c1
Pavel <klon_008@mail.ru>
2025-06-18 20:14:07 +0700

    gh-pages3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/Path-of-Exile---Betrayal-Cheat-Sheet-3.26/',
+    base: '/dist/',
 })
