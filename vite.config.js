252bcec6d50105f64361b97e7390f18b7699e299
Pavel <klon_008@mail.ru>
2025-06-18 20:12:16 +0700

    try gh-pages 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/react-betrayal-table/',
+    base: '/Path-of-Exile---Betrayal-Cheat-Sheet-3.26/',
 })
